6eebf1b905e3fdfb5203473b21fcae722bc3fa30
f0lg0 <folgonileonardo@gmail.com>
2022-01-08 18:57:07 +0100

    new: simple client test

--- src/client.py
+++ src/client.py
@@ -4,16 +4,23 @@ from time import sleep
 HOST = "0.0.0.0"
 PORT = 1234
 
-clients = []
+def send_msg(s, msg):
+	final = f"{len(msg):04d}{msg}"
+	s.sendall(bytes(final, encoding="UTF-8"))
 
-for i in range(0, 10):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	clients.append(s)
 
-	s.connect((HOST, PORT))
-	s.sendall(b"0005hello")
-	sleep(0.5)
+def main():
+	clients = []
 
-for c in clients:
-	c.close()
-	sleep(0.5)
+	for i in range(0, 10):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		clients.append(s)
+
+		s.connect((HOST, PORT))
+		send_msg(s, f"hello #{i}")
+
+	for c in clients:
+		c.close()
+
+if __name__ == "__main__":
+	main()
